79f7cca0e5e9d104c2ec29952a9bcec3f0ccf9eb
Indhumathi-SivaShanmugam <indhusmis@gmail.com>
2025-03-26 15:37:36 +0530

    PWA service worker check

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ if (rootElement) {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("/service-worker.js")
+
     .then(() => console.log("✅ Service Worker Registered"))
     .catch((error) => console.error("❌ Service Worker Registration Failed", error));
 }
